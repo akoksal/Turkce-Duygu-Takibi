7d18604f110837d8cb0a74088b8712aa65628558
akoksal <abdullatifkoksal@gmail.com>
2020-07-31 21:10:19 +0300

    Tweetscraper bocek stratejisinin gelistirilmesi

--- Sorgu/tweetscraper_bocek.py
+++ Sorgu/tweetscraper_bocek.py
@@ -6,17 +6,17 @@ import json
 import sys
 import pandas as pd
 import argparse
-from datetime import datetime
+import datetime
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--query')
 parser.add_argument('--fp')
-parser.add_argument('--limit', default=1000, type=int)
+parser.add_argument('--limit', default=1250, type=int)
 args = parser.parse_args()
 
 query = args.query
 target_fp = args.fp
-data_limit = args.limit
+data_limit = int(args.limit)
 
 print("Query", query)
 print("Filepath", target_fp)
@@ -27,12 +27,29 @@ if os.path.exists("TweetScraper/Data/"):
     shutil.rmtree("TweetScraper/Data/")
     print("Removed previous data")
 
-
-print(f"{query}, {target_fp} klasörüne çekiliyor.")
-commands = ["scrapy", "crawl", "TweetScraper", "-a", "top_tweet=True", "-a", f"query=\"{query}\"", "-s", f"CLOSESPIDER_ITEMCOUNT={data_limit}"]
+today = datetime.date.today()
+since = (today-datetime.timedelta(30)).strftime("%Y-%m-%d")
+print(f"{query}, {target_fp} klasörüne top twitler çekiliyor.")
+commands = ["scrapy", "crawl", "TweetScraper", "-a", "top_tweet=True", "-a", f"query=\"{query}\" since:{since}", "-s", f"CLOSESPIDER_ITEMCOUNT={2*data_limit//5}"]
 process = Popen(commands, stdout=PIPE, stderr=STDOUT, universal_newlines=True, cwd="TweetScraper/")
 for line in iter(process.stdout.readline, ''):
     print(line)
+process.kill()
+
+total_day = 30
+total_data = 3*data_limit//5
+for i in range(total_day):
+    day = (today-datetime.timedelta(i)).strftime("%Y-%m-%d")
+    next_day = (today-datetime.timedelta(i-1)).strftime("%Y-%m-%d")
+    print(f"{query}, {target_fp} klasörüne en yakın twitler {day}-{next_day} günler arasında olacak şekilde toplam {total_data//total_day} twit çekilecek.")
+
+    commands = ["scrapy", "crawl", "TweetScraper", "-a", f"query=\"{query} since:{day} until:{next_day} \"", "-s", f"CLOSESPIDER_ITEMCOUNT={total_data//total_day}"]
+    process = Popen(commands, stdout=PIPE, stderr=STDOUT, universal_newlines=True, cwd="TweetScraper/")
+    for line in iter(process.stdout.readline, ''):
+        print(line)
+
+    process.kill()
+
 
 dataset = []
 for el in os.listdir("TweetScraper/Data/tweet"):
