80465dd1a5275c23fc3dcfd268943b1a532a3c44
Abdullatif Köksal <abdullatifkoksal@gmail.com>
2020-09-20 11:08:46 +0300

    TweetScrapper'dan resmi Twitter API'ya geçiş

--- Sorgu/query.sh
+++ Sorgu/query.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$#" -ne 3 ]; then
-    python3 tweetscraper_bocek.py --query "$1" --fp "$2"
+    python3 twitter_bocek.py --query "$1" --fp "$2"
 else
-    python3 tweetscraper_bocek.py --query "$1" --fp "$2" --limit "$3"
+    python3 twitter_bocek.py --query "$1" --fp "$2" --limit "$3"
 fi
 
 python3 onnx_tahmin.py "$2"
